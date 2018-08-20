793108b8bef046103d15532292a60fef859358e8
bonzaiferroni <giPloki90()>
2018-08-20 16:04:37 -0600

    first commit

---
+++ Core/Fusion.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FusionLib.Core
+{
+    public class Fusion
+    {
+        public static Fusion Create(string name, Action<Fusion> addComponents)
+        {
+            var fuser = Create(name);
+            fuser.Add(addComponents);
+            return fuser;
+        }
+
+        private static Fusion Create(string name)
+        {
+            return new Fusion(name);
+        }
+
+        public static Fusion Mount(GameObject go)
+        {
+            return new Fusion(go);
+        }
+
+        protected Fusion(GameObject go)
+        {
+            Go = go;
+        }
+        
+        protected Fusion(string name, params Type[] components)
+        {
+            Go = new GameObject(name, components);
+        }
+
+        public string Name => Go.name;
+        public Transform Transform => Go.transform;
+        public RectTransform Rect => Go.GetComponent<RectTransform>();
+        
+        public GameObject Go { get; }
+
+        public Fusion Add(Action<Fusion> addComponents)
+        {
+            try
+            {
+                addComponents(this);
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"Exception adding components to {Name}:\n{e}");
+            }
+            
+            return this;
+        }
+
+        public T Add<T>(Action<T> configure = null) where T : Component
+        {
+            var comp = Go.AddComponent<T>();
+            if (comp == null) throw new Exception($"{Name} couldn't add a component: {typeof(T).Name}");
+            configure?.Invoke(comp);
+            return comp;
+        }
+
+        public T Get<T>(Action<T> configure = null) where T : Component
+        {
+            var comp = Go.GetComponent<T>();
+            if (comp == null) throw new Exception($"{Name} couldn't find a {typeof(T)}");
+            configure?.Invoke(comp);
+            return comp;
+        }
+
+        public T Get<T>(string tag, Action<T> configure = null) where T : Component
+        {
+            var child = GetChild(tag);
+            return child.Get(configure);
+        }
+
+        public Fusion GetChild(string tag)
+        {
+            var stack = new Stack<Transform>();
+            stack.Push(Transform);
+
+            while (stack.Count > 0)
+            {
+                var transform = stack.Pop();
+                if (transform.name == tag)
+                {
+                    return new Fusion(transform.gameObject);
+                }
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    stack.Push(transform.GetChild(i));
+                }
+            }
+
+            throw new Exception($"{Name} could find child: {tag}");
+        }
+
+        public Fusion NewChild(string tag, Action<Fusion> addParts)
+        {
+            var child = Create(tag);
+            child.Transform.SetParent(Transform);
+            child.Add(addParts);
+            return child;
+        }
+    }
+}
---
+++ Tests/FuserTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Codex.Tests
+{
+    public class FuserTests
+    {
+        /*[Test]
+        public void Constructor_CreateAGameObject()
+        {
+            var fuser = new Fuser("test");
+            Assert.NotNull(fuser.Go);
+        }
+
+        [Test]
+        public void Add_AddsComponent()
+        {
+            var fuser = new Fuser("test");
+            fuser.Add<Text>();
+            Assert.NotNull(fuser.Go.GetComponent<Text>());
+        }
+
+        [Test]
+        public void Add_AddsGameObject()
+        {
+            var fuser = new Fuser("test");
+            var child = fuser.AddChild("child");
+            Assert.AreEqual(1, fuser.Transform.childCount);
+        }
+
+        [Test]
+        public void Add_AddInHierarchy()
+        {
+            var fuser = new Fuser("Button")
+                .Add<Button>(x => x.interactable = false)
+                .AddChild("Label", f => f
+                    .Add<Text>(x => x.color = Color.white)
+                )
+                .Add<Image>(x => x.color = Color.black);
+            Assert.NotNull(fuser.Go.GetComponent<Button>());
+            Assert.NotNull(fuser.Go.GetComponent<Image>());
+            Assert.NotNull(fuser.Transform.GetChild(0).GetComponent<Text>());
+        }*/
+    }
+}
