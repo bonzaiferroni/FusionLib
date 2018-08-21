b6566299910a7a9fea261d4a932e9089f3a19688
bonzaiferroni <giPloki90()>
2018-08-20 20:57:04 -0600

    implemented object pooling

---
+++ Core/PoolMember.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace FusionLib.Core
+{
+    public class PoolMember : MonoBehaviour
+    {
+        public Type Type { get; private set; }
+        public RecipeFactory Factory { get; private set; }
+        
+        public void Init(Type type, RecipeFactory recipeFactory)
+        {
+            Type = type;
+            Factory = recipeFactory;
+        }
+
+        public void Return()
+        {
+            Factory.Return(Type, this);
+        }
+
+    }
+}
--- Core/RecipeFactory.cs
+++ Core/RecipeFactory.cs
@@ -1,20 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace FusionLib.Core
 {
     public abstract class RecipeFactory
     {
+        private readonly Dictionary<Type, GameObject> _prototypes = new Dictionary<Type, GameObject>();
+        private readonly Dictionary<Type, Stack<GameObject>> _pools = new Dictionary<Type, Stack<GameObject>>();
+
+        private Transform _prototypesParent;
+        private Transform _pooledParent;
+
         protected abstract Dictionary<Type, Action<Fusion>> Recipes { get; }
+
+        public RecipeFactory()
+        {
+            _prototypesParent = new GameObject("Prototypes").transform;
+            _pooledParent = new GameObject("Pooled").transform;
+            CreatePrototypes();
+        }
+
+        private void CreatePrototypes()
+        {
+            foreach (var kvp in Recipes)
+            {
+                var recipe = kvp.Value;
+                var type = kvp.Key;
+                var go = Fusion.Create(type.Name, recipe).Go;
+                go.transform.SetParent(_prototypesParent);
+                _prototypes[type] = go;
+                _pools[type] = new Stack<GameObject>();
+            }
+        }
 
         public T GetView<T>(Type type, Action<T> init) where T : Component
         {
             if (!Recipes.ContainsKey(type)) throw new Exception($"You don't have a recipe for {type.Name}!");
-            var recipe = Recipes[type];
-            var fusion = Fusion.Create(type.Name, recipe);
-            var view = fusion.Get<T>();
-            init(view);
+            var pool = _pools[type];
+            
+            T view;
+            if (pool.Count > 0)
+            {
+                var go = pool.Pop();
+                view = go.GetComponent<T>();
+                Debug.Log("found apool guy!");
+            }
+            else
+            {
+                var go = Object.Instantiate(_prototypes[type]);
+                view = go.GetComponent<T>();
+                init(view);
+                var member = go.GetComponent<PoolMember>();
+                if (member) member.Init(type, this);
+            }
+            
             return view;
         }
         
@@ -22,6 +63,15 @@ namespace FusionLib.Core
         {
             var type = typeof(T);
             return  GetView(type, init);
+        }
+
+        public void Return(Type type, PoolMember poolMember)
+        {
+            if (!Recipes.ContainsKey(type)) 
+                throw new Exception($"Returned poolmember for unregistered type: {type.Name}");
+            _pools[type].Push(poolMember.gameObject);
+            poolMember.transform.SetParent(_pooledParent);
+            Debug.Log("returned apool guy!");
         }
     }
 }
