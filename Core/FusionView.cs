3ba12ec9cf4cf9afc398b4e4441d6695f36c7c70
bonzaiferroni <giPloki90()>
2018-08-20 18:40:16 -0600

    refactor RecipeFactory

---
+++ Core/FusionView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace FusionLib.Core
+{
+    public abstract class FusionView<U> : MonoBehaviour
+    {
+        public U Bundle { get; private set; }
+
+        public virtual void Init(U bundle)
+        {
+            Bundle = bundle;
+        }
+
+        protected T GetChild<T>(string componentTag) where T : Component
+        {
+            T comp = null;
+            foreach (var component in GetComponentsInChildren<T>())
+            {
+                if (component.name == componentTag)
+                {
+                    comp = component;
+                    break;
+                }
+            }
+            return Validate(comp);
+        }
+
+        protected T GetChild<T>() where T : Component
+        {
+            return Validate(GetComponentInChildren<T>());
+        }
+
+        protected T Get<T>() where T : Component
+        {
+            return Validate(GetComponent<T>());
+        }
+
+        private T Validate<T>(T comp) where T : Component
+        {
+            if (!comp) throw new Exception($"{GetType().Name} couldn't find a component: {typeof(T)}");
+            return comp;
+        }
+    }
+}
---
+++ Core/RecipeFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FusionLib.Core
+{
+    public abstract class RecipeFactory<U>
+    {
+        protected abstract Dictionary<Type, Action<Fusion>> Recipes { get; }
+
+        protected abstract void Init(FusionView<U> view, U bundle);
+
+        public Fusion GetView(Type type)
+        {
+            if (!Recipes.ContainsKey(type)) throw new Exception($"You don't have a recipe for {type.Name}!");
+            var recipe = Recipes[type];
+            return Fusion.Create(type.Name, recipe);
+        }
+        
+        public T GetView<T>(U bundle) where T : FusionView<U>
+        {
+            var type = typeof(T);
+            var fusion = GetView(type);
+            
+            var view = fusion.Get<T>();
+            BaseInit(view, bundle);
+            Init(view, bundle);
+            return view;
+        }
+        
+        protected void BaseInit<T>(T view, U bundle) where T : FusionView<U>
+        {
+            foreach (var fusionView in view.GetComponentsInChildren<FusionView<U>>())
+            {
+                fusionView.Init(bundle);
+            }
+        }
+    }
+}
