b538b0b94b979093fb9a9d93f026dc992f23b9c9
bonzaiferroni <giPloki90()>
2018-08-20 23:03:19 -0600

    implemented CookView

---
+++ Core/CookView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace FusionLib.Core
+{
+    [ExecuteInEditMode]
+    public abstract class CookView : MonoBehaviour
+    {
+        protected abstract Action<Fusion> Recipe { get; }
+
+        private void OnEnable()
+        {
+            var fusion = Fusion.Mount(gameObject);
+            fusion.Add(Recipe);
+            DestroyImmediate(this);
+        }
+    }
+}
